63023b12c9f2cd57475a37f61ec738e707efd529
panaghiageorge <38526106+panaghiageorge@users.noreply.github.com>
2018-08-14 13:08:16 +0300

    Retrofit2
    Task 12

---
+++ Retrofit2/app/src/main/java/com/example/user/retrofit2/CreateUserResponse.java
@@ -0,0 +1,13 @@
+package com.example.user.retrofit2;
+
+import com.google.gson.annotations.SerializedName;
+public class CreateUserResponse {
+    @SerializedName("name")
+    public String name;
+    @SerializedName("job")
+    public String job;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("createdAt")
+    public String createdAt;
+}
